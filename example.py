9846397728f4746079d492c5597bd67b1ce223af
MikayelyanVahe <vahe.mikayelyan80@gmail.com>
2021-02-17 21:49:48 +0400

    code changes ver 0.7

--- example.py
+++ example.py
@@ -4,9 +4,8 @@ import sys
 
 if __name__ == '__main__':
 
-    text = "az acr repository list -n commodity"
-    cmd_output = subprocess.check_output(["powershell", "-Command", text], text=True)
-    print(type(cmd_output))
-#    newlist = cmd_output.split(maxsplit=1)
-    for items in cmd_output:
-        print(items)
+    isTrue: bool = True
+    if isTrue:
+        print("Yes")
+    else:
+        print("No")
--- get_keyvault_secret.py
+++ get_keyvault_secret.py
@@ -25,17 +25,32 @@ def welcome_menu():
 
 
 def main_menu():
-    print("\nCreate a Keyvault [c] / Create KeyVault secret:value/s [s]")
+    print("\nCreate a Keyvault [c] / Create KeyVault secret:value(s) [s]")
     print("List KeyVault secret names [ls] / Show secret/value pair [sh]")
     print("Backup/restote Keyvault [br]")
     print("Delete Keyvault [dk] / delete secret [ds]")
     print("Quit program [q]")
 
 
-def azure_keyvault_set_create_secret():
+def check_azure_keyvault(keyvaultname: str, rg: str):
+    entrylist = []
+    txt = "az keyvault list --resource-group {} --resource-type vault"
+    cmd = txt.format(rg)
+    command = subprocess.check_output(["powershell", "-Command", cmd], text=True)
+    commandoutputlist = json.loads(command)
+    for items in commandoutputlist:
+        vault = items.get("name")
+        entrylist.append(vault)
+    if keyvaultname in entrylist:
+        return True
+    else:
+        return False
+
+
+def azure_keyvault_set_create_secret(keyvaultname: str):
     secretdict: dict = {}
     secretcount = input("Please enter how many secrets you want to create: ")
-    keyvaultname = input("Please type vault name: ")
+    # keyvaultname = input("Please type vault name: ")
     for x in range(int(secretcount)):
         secretname = input("\nPlease type secret name: ")
         secretvalue = getpass.getpass("Enter secret value: ", stream=None)
@@ -48,7 +63,7 @@ def azure_keyvault_set_create_secret():
         if command.returncode != 0:
             print("An error occurred: %s", command.stderr)
         else:
-            print("SECRET " + key + " SUCCESSFULLY CREATE IN " + value)
+            print("SECRET " + key + " SUCCESSFULLY CREATE with " + value)
 
 
 def azure_keyvault_delete_secret(keyvaultname: str):
@@ -58,7 +73,7 @@ def azure_keyvault_delete_secret(keyvaultname: str):
     for items in secretlist:
         print(str(i) + ". " + items)
         i = i + 1
-    deletesecretsamount = input("\nPlease enter how many secrets you want to delete: ")
+    deletesecretsamount = input("\nPlease enter how many secrets you want to delete from : " + keyvaultname + "keyvault")
     for x in range(int(deletesecretsamount)):
         secretname = input("Please enter secret name do delete: ")
         txt = "az keyvault secret delete --vault-name {} -n {}"
@@ -95,6 +110,7 @@ def azure_keyvault_secret_show_by_secret_name(keyvaultname: str):
 def azure_keyvault_secret_show(keyvaultname: str):
     inputlist = azure_keyvault_secret_list(keyvaultname)
     keyvultsecretdict = {}
+    i: int = 1
     for item in inputlist:
         txt = "az keyvault secret show --vault-name {} -n {}"
         cmd = txt.format(keyvaultname, item)
@@ -103,17 +119,18 @@ def azure_keyvault_secret_show(keyvaultname: str):
         secret_value = jsonloader["value"]
         keyvultsecretdict[item] = secret_value
     for key, value in keyvultsecretdict.items():
-        print("\nSECRET NAME: " + key + "\nSECRET VALUE IS: " + value)
+        print("\n" + str(i) + ". SECRET NAME: " + key + "\nSECRET VALUE IS: " + value)
+        i = i + 1
 
 
-def azure_keyvault_create(keyvaultname, rg):
+def azure_keyvault_create(keyvaultname: str, rg: str):
     location = input("Please type location (default is westeurope): ") or "westeurope"
     sku = input("Please type sku (default is standart): ") or "standard"
     txt = "az keyvault create -g {} -n {} -l {} --sku {}"
     cmd = txt.format(rg, keyvaultname, location, sku)
     command = subprocess.Popen(["powershell", "-Command", cmd], text=True)
     while command.poll() is None:
-        print("--Running--")
+        print("--Creating--")
         sleep(10)
     if command.returncode != 0:
         print("An error occurred: %s", command.stderr)
@@ -128,7 +145,7 @@ def azure_keyvault_delete(keyvaultname: str, rg: str):
     cmd = txt.format(keyvaultname, rg)
     command = subprocess.Popen(["powershell", "-Command", cmd], text=True)
     while command.poll() is None:
-        print("--Running--")
+        print("--Deleting--")
         sleep(10)
     if command.returncode != 0:
         print("An error occurred: %s", command.stderr)
@@ -137,7 +154,7 @@ def azure_keyvault_delete(keyvaultname: str, rg: str):
 
 
 def close_program():
-    print("Script will be interrupted")
+    print("\nScript will be interrupted")
     sys.exit()
 
 
@@ -155,22 +172,51 @@ if __name__ == '__main__':
 
         # Create Keyvault block
         if answer == "c":
-            source_rg = input("\nPlease type RG you want to create " + "(default is " + default_rg + ")") or default_rg
+            source_rg = input("\nPlease type RG you want to create " + "(default is " + default_rg + "): ") or default_rg
             source_keyvaultname = input("Please type vault name: ")
-            azure_keyvault_create(source_keyvaultname, source_rg)
-            input("\n Press any to retrun main menu: ")
-            trigger = False
+            print("Checking if " + source_keyvaultname + " already exist in " + source_rg + " Resource group")
+            is_keyvault_exist = check_azure_keyvault(source_keyvaultname, source_rg)
+            print(is_keyvault_exist)
+            if is_keyvault_exist:
+                print("\nKeyvault " + source_keyvaultname + " already exist")
+                input("Press enter to return main menu: ")
+                trigger = False
+            else:
+                azure_keyvault_create(source_keyvaultname, source_rg)
+                input("\n Press enter to return main menu: ")
+                trigger = False
 
         elif answer == "dk":
             source_rg = input("\nPlease type RG you want to create (default is cargoo-kv-rg-weu1): ") \
                         or "cargoo-kv-rg-weu1"
-
+            source_keyvaultname = input("Please type vault name: ")
+            azure_keyvault_delete(source_keyvaultname, source_rg)
+            input("\nPress enter to return main menu: ")
             trigger = False
+
+        elif answer == "s":
+            if not source_keyvaultname:
+                source_keyvaultname = input("Please type vault name: ")
+                azure_keyvault_set_create_secret(source_keyvaultname)
+                input("\nPress enter to return main menu: ")
+                trigger = False
+            else:
+                source_keyvaultname = input("Please type vault name " +
+                                            "(or press enter to use current keyvault " + source_keyvaultname + " :") \
+                                      or source_keyvaultname
+                azure_keyvault_set_create_secret(source_keyvaultname)
+                input("\nPress enter to return main menu: ")
+                trigger = False
+
+        elif answer == "ls":
+            source_rg = input("\nPlease type RG you want to create (default is cargoo-kv-rg-weu1): ") \
+                        or "cargoo-kv-rg-weu1"
+            source_keyvaultname = input("Please type vault name: ")
 
         elif answer == "q":
             close_program()
             trigger = True
 
         else:
-            print("there is no such oparation key")
+            print("there is no such oparation key defined")
             trigger = False
